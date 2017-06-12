c864a588ef23df51d0794cb7181aedfb7f41f7bb
Satveer Singh <satveer@satveers-mbp.fios-router.home>
2017-06-12 00:33:16 -0500

    Auto Layout fix

---
+++ MemeMe 1.0/TopBottomDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  TopBottomDelegate.swift
+//  MemeMe 1.0
+//
+//  Created by Satveer Singh on 6/11/17.
+//  Copyright © 2017 Satveer Singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopBottomDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    
+}
--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var actualImage: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func TakeImgeByCamera(_ sender: Any) {
+        print("take image by camera")
+    }
 
+    @IBAction func PickImageFromAlbum(_ sender: Any) {
+        print("pick image from album")
+    }
+    
+    @IBAction func ShareMeme(_ sender: Any) {
+    print("share meme")
+    }
+    @IBAction func ResetMemeView(_ sender: Any) {
+        self.topText.text = " "
+        self.bottomText.text = " "
+        print("reset meme veiw")
+    
+    }
 
 }
 
